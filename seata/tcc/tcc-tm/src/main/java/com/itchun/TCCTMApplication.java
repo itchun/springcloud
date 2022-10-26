7b3be0a92cea8d403b41ceb1c212bae3829f2a5a
王春 <itchun@163.com>
2022-10-26 23:32:37 +0800

    Seata部分

--- seata/tcc/tcc-tm/src/main/java/com/itchun/TCCTMApplication.java
+++ seata/tcc/tcc-tm/src/main/java/com/itchun/TCCTMApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class TCCTMApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(TCCTMApplication.class, args);
 	}
 
 }
