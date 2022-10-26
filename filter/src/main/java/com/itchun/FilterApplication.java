f83da15b76a04aacab5a7b5594a9278fc57c59de
王春 <itchun@163.com>
2022-10-26 23:35:07 +0800

    Application部分

--- filter/src/main/java/com/itchun/FilterApplication.java
+++ filter/src/main/java/com/itchun/FilterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class FilterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(FilterApplication.class, args);
 	}
 
 }
--- hikariDataSource/src/main/java/com/itchun/HikarDataSourceApplication.java
+++ hikariDataSource/src/main/java/com/itchun/HikarDataSourceApplication.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class Application {
+public class HikarDataSourceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(HikarDataSourceApplication.class, args);
 	}
 
 }
