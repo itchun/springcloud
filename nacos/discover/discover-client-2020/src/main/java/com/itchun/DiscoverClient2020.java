d0198b5b08a535c36702f8819de7f50e6009635c
王春 <itchun@163.com>
2022-12-15 10:37:37 +0800

    提交

---
+++ nacos/discover/discover-client-2020/src/main/java/com/itchun/DiscoverClient2020.java
@@ -0,0 +1,13 @@
+package com.itchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverClient2020 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverClient2020.class, args);
+	}
+
+}
